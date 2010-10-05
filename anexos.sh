5cec1d6d0ab18b11ce8b775e34f9d4deeb78fb66
William da Silva Vianna <williamvianna@gmail.com>
2010-10-05 19:56:23 -0300

    script melhorado e inclluído vários links para download

--- anexos.sh
+++ anexos.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-mkdir videos
-cd videos
-wget -c http://www.antispam.br/videos/cgi-navegar-legendado.wmv
-wget -c http://www.antispam.br/videos/cgi-invasores-legendado.wmv
-wget -c http://www.antispam.br/videos/cgi-spam-legendado.wmv
-wget -c http://www.antispam.br/videos/cgi-defesa-legendado.wmv
-cd ..
 
-mkdir doc
-cd doc
-wget -c http://cartilha.cert.br/download/cartilha-seguranca-internet.pdf
+#este script foi desenvolvido para ser executado no GNU/Linux
 
+g=`which wget`
+
+if [ "$g" != "" ]
+then
+  echo ######################################
+  echo "Usando o $g para download dos anexos"
+  echo ######################################
+  sleep 2
+  mkdir videos
+  cd videos
+  $g -c http://www.antispam.br/videos/cgi-navegar-legendado.wmv
+  $g -c http://www.antispam.br/videos/cgi-invasores-legendado.wmv
+  $g -c http://www.antispam.br/videos/cgi-spam-legendado.wmv
+  $g -c http://www.antispam.br/videos/cgi-defesa-legendado.wmv
+  cd ..
+  mkdir doc
+  cd doc
+  $g -c http://cartilha.cert.br/download/cartilha-seguranca-internet.pdf
+else
+  echo ################
+  echo "Instale o wget"
+  echo ################
+  exit 1
+fi
