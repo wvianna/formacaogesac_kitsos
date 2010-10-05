597e7cf310fc1005469ec8adca5ddd701b3d06b5
William da Silva Vianna <williamvianna@gmail.com>
2010-10-04 23:47:11 -0300

    script para download dos anexos

--- anexos.sh
+++ anexos.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+mkdir videos
+cd videos
+wget -c http://www.antispam.br/videos/cgi-navegar-legendado.wmv
+wget -c http://www.antispam.br/videos/cgi-invasores-legendado.wmv
+wget -c http://www.antispam.br/videos/cgi-spam-legendado.wmv
+wget -c http://www.antispam.br/videos/cgi-defesa-legendado.wmv
+cd ..
+
+mkdir doc
+cd doc
+wget -c http://cartilha.cert.br/download/cartilha-seguranca-internet.pdf
+
